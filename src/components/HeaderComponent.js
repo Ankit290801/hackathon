4e5740818ddb88ab53705c2567e3eb8fa689a3ce
Bhaskar Sengupta <sajal1943@gmail.com>
2020-10-30 14:41:47 +0530

    aagun added with bugs

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, Collapse, NavbarToggler, Jumbotron,
+import { Spinner , Alert , Navbar, NavbarBrand, Nav, NavItem, Collapse, NavbarToggler, Jumbotron,
         Button, Modal, ModalHeader, ModalBody, Form, Label, Input, FormGroup } from 'reactstrap';
 import {NavLink} from 'react-router-dom';
-
+import fire from './fire';
 class Header extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             isNavOpen : false,
-            isModalOpen : false
+            isModalOpen : false,
+            isUserLoggedIn: false
         };
         this.toggleNav = this.toggleNav.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
         this.handleLogin = this.handleLogin.bind(this);
+        this.LogOut = this.LogOut.bind(this);
+        this.toggleLogged = this.toggleLogged.bind(this);
+        this.userStatus = this.userStatus.bind(this);
     }
 
     toggleNav(){
@@ -27,33 +31,115 @@ class Header extends Component {
             isModalOpen : !this.state.isModalOpen
         });
     }
-
+    toggleLogged(){
+        this.setState({
+            isUserLoggedIn: !this.state.isUserLoggedIn
+        });
+        return(
+            <Alert>logged in</Alert>
+        );
+    }
+    
     handleLogin(event){
         this.toggleModal();
-        alert("Username: " + this.username.value + " Password: " + this.password.value + " Remember: " + this.remember.checked);
+        //alert("Username: " + this.username.value + " Password: " + this.password.value + " Remember: " + this.remember.checked);
+        fire
+        .auth()
+        .signInWithEmailAndPassword(this.username.value,this.password.value)
+        .then(cred=>{
+            console.log(cred);
+            this.toggleLogged();
+          
+           
+        })
+        .catch(err=>{
+        console.log(err);
+        });
+        
+        
         event.preventDefault();
+
+    }
+    LogOut(e){
+        e.preventDefault();
+        
+        fire.auth()
+        .signOut()
+        .then(()=>{
+            console.log('user logged out');
+            this.toggleLogged();
+        })
+        .catch(err=>{
+           console.log(err);
+        })
+       
+    }
+    getStyle=()=>{
+        if(this.state.isUserLoggedIn)
+        {
+            return {
+                display:'block'
+            }
+        }else{
+            return{
+                display:'none'
+            }
+        }
     }
 
+    getSpinnerStyle=()=>{
+        if(this.state.isUserLoggedIn)
+        {
+            return {
+                width: '3rem', height: '3rem' ,position:'relative',top:'50%',left:'50%',
+                 display:'block'
+            }
+        }else{
+            return{
+                width: '3rem', height: '3rem' ,position:'relative',top:'50%',left:'50%',
+                display:'none'
+            }
+        }
+    }
+
+    userLoggedOut=()=>{
+        if(!this.state.isUserLoggedIn)
+        {
+            return {
+                display:'block'
+            }
+        }else{
+            return{
+                display:'none'
+            }
+        }
+    }
+    userStatus()
+    {
+        fire.auth().onAuthStateChanged(user=>{
+            console.log(user);
+        })
+    }
     render(){
         return(
-            <>
+            <>  
                 <Navbar light expand="md">
                 <div className="container">
                     <NavbarToggler onClick={this.toggleNav}></NavbarToggler>
                     <NavbarBrand className="mr-auto" href="/">Hackathon</NavbarBrand>
                     <Collapse isOpen={this.state.isNavOpen} navbar>
                     <Nav navbar>
-                        <NavItem>
+                        <NavItem style={this.getStyle()}>
                             <NavLink className="nav-link" to="/home">
-                                <span className="fa fa-home fa-lg"></span>Home
+                                <span  className="fa fa-home fa-lg"></span>Home
                             </NavLink>
                         </NavItem>
-                        <NavItem>
+                        <NavItem style={this.getStyle()}>
                             <NavLink className="nav-link" to="/aboutus">
                                 <span className="fa fa-info fa-lg"></span>About Us
                             </NavLink>
                         </NavItem>
-                        <NavItem>
+                        <NavItem style={this.getStyle()}>
                             <NavLink className="nav-link" to="/contact">
                                 <span className="fa fa-address-card fa-lg">Contact</span>
                             </NavLink>
@@ -61,14 +147,21 @@ class Header extends Component {
                     </Nav>
                     <Nav className="ml-auto" navbar>
                         <NavItem>
-                            <Button outline onClick={this.toggleModal} className="m-2">
+                            <Button outline style={this.userLoggedOut()} onClick={this.toggleModal} className="m-2">
                                 <span className="fa fa-sign-in fs-lg"></span> Login
                             </Button>
                         </NavItem>
                         <NavItem>
                             <NavLink className="nav-link" to="/register">
-                                <Button outline>
+                                <Button outline style={this.userLoggedOut()}>
                                     <span className="fa fa-user-plus fs-lg"></span>Sign Up
+                                </Button>
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="/register">
+                                <Button outline style={this.getStyle()} onClick={this.LogOut} >
+                                    <span className="fa fa-user-plus fs-lg"></span> LogOut
                                 </Button>
                             </NavLink>  
                         </NavItem>
@@ -76,7 +169,7 @@ class Header extends Component {
                     </Collapse>
                 </div>
                 </Navbar>
-                <Jumbotron>
+                <Jumbotron style={this.getStyle()} >
                     <div className="container">
                         <div className="row row-header">
                             <div className="col-12 col-sm-6">
@@ -86,6 +179,8 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                <Spinner  style={this.getSpinnerStyle()} />{' '}
+                
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
                     <ModalBody>
